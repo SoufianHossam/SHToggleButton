08725f4392de6a2faacd31dcf350c4206a7969ec
soufian <soufian.el.hossary@gmail.com>
2018-09-18 15:26:01 +0200

    Initial Commit

---
+++ SHToggleButton/SHToggleButton.swift
@@ -0,0 +1,121 @@
+//
+//  SHToggleButton.swift
+//  SHToggleButton
+//
+//  Created by Soufian Hossam on 9/18/18.
+//  Copyright © 2018 Soufian Hossam. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class SHToggleButton: UIButton {
+    // MARK: - Variables
+    private var fallingImage: UIImage?
+    
+    // MARK: - IBInspectable
+    @IBInspectable var defaultImage: UIImage? {
+        didSet {
+            setImage(defaultImage, for: .normal)
+        }
+    }
+    
+    @IBInspectable var selectedImage: UIImage? {
+        didSet {
+            setImage(selectedImage, for: .selected)
+        }
+    }
+    
+    @IBInspectable var imageToAnimate: UIImage? {
+        didSet {
+            fallingImage = imageToAnimate
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    private func setup() {
+        isSelected = false
+        addTarget(self, action: #selector(tap), for: .touchUpInside)
+    }
+    
+    @objc private func tap() {
+        isSelected = !isSelected
+        
+        if isSelected {
+            bounceAnimation()
+            fallingAnimation()
+        }
+    }
+    
+    private func bounceAnimation() {
+        AnimationBuilder(for: self)
+            .scaleX(1.5)
+            .scaleY(1.5)
+            .execute(type: .permanent, completion: {
+                AnimationBuilder(for: self)
+                    .duration(0.8)
+                    .damping(0.4)
+                    .options(.allowUserInteraction)
+                    .execute(type: .permanent)
+            })
+    }
+    
+    private func fallingAnimation() {
+        let x = frame.width / 2
+        let y = frame.height / 2
+        
+        for _ in 0...3 {
+            let duration = 0.5 + drand48() * 0.5
+            
+            let imageView = UIImageView(image: fallingImage)
+            imageView.frame = CGRect(x: x, y: y, width: frame.width * 0.8, height: frame.height * 0.8)
+            
+            addSubview(imageView)
+            
+            let animation = CAKeyframeAnimation(keyPath: "position")
+            animation.path = createFallingPath(center: CGPoint(x: x, y: y))
+            animation.duration = duration
+            animation.fillMode = kCAFillModeForwards
+            animation.isRemovedOnCompletion = false
+            animation.timingFunctions = [CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)]
+            
+            imageView.layer.add(animation, forKey: nil)
+            
+            UIView.animate(withDuration: duration, animations: {
+                imageView.alpha = 0
+            }, completion: { success in
+                imageView.removeFromSuperview()
+            })
+        }
+    }
+    
+    private func createFallingPath(center: CGPoint) -> CGPath {
+        let lowerValue = -50
+        let upperValue = 50
+        let random = Double(Int(arc4random_uniform(UInt32(upperValue - lowerValue + 1))) + lowerValue)
+        
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: center.x, y: center.y))
+        
+        let y = center.y + 200
+        let endPoint = CGPoint(x: center.x + CGFloat(random), y: y)
+        
+        let cp1 = CGPoint(x: Double(center.x) + random, y: Double(center.y) - 80)
+        let cp2 = CGPoint(x: Double(center.x) + random, y: Double(center.y) - 80)
+        
+        path.addCurve(to: endPoint, controlPoint1: cp1, controlPoint2: cp2)
+        path.stroke()
+        path.lineWidth = 3
+        
+        return path.cgPath
+    }
+}
